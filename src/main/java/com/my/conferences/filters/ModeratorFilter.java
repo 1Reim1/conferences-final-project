b450642d559ee995d005249a0320023e8f385b4f
Reim <timereim@gmail.com>
2022-09-15 14:37:04 +0300

    ModeratorFilter.java

--- src/main/java/com/my/conferences/filters/ModeratorFilter.java
+++ src/main/java/com/my/conferences/filters/ModeratorFilter.java
@@ -2,24 +2,22 @@ package com.my.conferences.filters;
 
 import com.my.conferences.entity.User;
 import jakarta.servlet.*;
-import jakarta.servlet.annotation.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "auth")
-public class AuthFilter implements Filter {
+@WebFilter(filterName = "moderator")
+public class ModeratorFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
-        if (user == null) {
-            response.sendRedirect("auth");
+        User user = (User) request.getSession().getAttribute("user");
+        if (user.getRole() != User.Role.MODERATOR) {
+            response.sendRedirect("home");
             return;
         }
 
